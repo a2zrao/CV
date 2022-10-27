8bfcab1dc3f86fe868b0e56336b9be79b26d50c4
cbooks <115538489+a2zrao@users.noreply.github.com>
2022-10-27 21:51:24 +0530

    Add files via upload

--- assets/vendors/@popperjs/core/index.d.ts
+++ assets/vendors/@popperjs/core/index.d.ts
@@ -1,1 +1,9 @@
-export * from './lib';
+import { Options, Modifier, Instance, VirtualElement } from "./types";
+export * from "./types";
+export * from "./enums";
+declare type PopperGeneratorArgs = {
+    defaultModifiers?: Array<Modifier<any>>;
+    defaultOptions?: Partial<Options>;
+};
+export declare function popperGenerator(generatorOptions?: PopperGeneratorArgs): (reference: Element | VirtualElement, popper: HTMLElement, options?: Partial<Options>) => Instance;
+export declare const createPopper: (reference: Element | VirtualElement, popper: HTMLElement, options?: Partial<Options>) => Instance;
